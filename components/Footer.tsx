4dfd1b25beea26cdd5b45915342bf98185268a56
Rajender <rajenderkatkuri123@gmail.com>
2022-08-12 21:34:56 +0530

    Stick footer to bottom

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,7 +5,7 @@ import BrandIcon from "./BrandIcon";
 
 const Footer = () => {
   return (
-    <div className="z-50 bg-black text-slate-300 w-full flex flex-col items-center justify-start p-4">
+    <footer className="relative bottom-0 z-50 bg-black text-slate-300 w-full flex flex-col items-center justify-start p-4">
       <h1 className="text-sm mb-2">Built with</h1>
       <div className="flex items-center justify-center space-x-2">
         <BrandIcon src={NextJSLogo} />
@@ -14,7 +14,7 @@ const Footer = () => {
       <h1 className="mt-2">
         by <span className="font-semibold text-white">Rajender Katkuri</span>
       </h1>
-    </div>
+    </footer>
   );
 };
 
